508f9ee553485531f9ba69737d2e4a545ddcb967
Peter G. Chang <peter0414@gmail.com>
2025-08-21 10:38:46 -0700

    Update __init__.py to include proton transfer functions

---
+++ chemistryinductivebias/__init__.py
@@ -0,0 +1,4 @@
+from .src.proton_transfer import proton_transfer_predict, MechanismOptions
+from .src.utils import show_candidates
+
+__all__ = ["proton_transfer_predict", "MechanismOptions", "show_candidates"]
